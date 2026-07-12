00f5198bc3ebf5c6f5863214f50d6b1b0c43434d
Dibakar <dibakarbera01@gmail.com>
2026-07-12 10:56:28 +0530

    type-casting python

---
+++ Fundamentals/03_type_casting.py
@@ -0,0 +1,24 @@
+a =34
+b="34"
+d=223
+
+print(a)
+print(type(a))
+
+print(b)
+print(type(b))
+
+# convert b to an integer 
+c = int(b)
+print(c)
+print(type(c))
+
+#convert d to an string
+e = str(d)
+print(e)
+print(type(e))
+
+#convert int to float
+f =13
+g=float(f)
+print(g)
