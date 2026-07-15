d399d0b1f116cbaf17a0a62e0b2cb8d303726297
Dibakar <dibakarbera01@gmail.com>
2026-07-15 22:09:38 +0530

    function

---
+++ Function/01_function.py
@@ -0,0 +1,17 @@
+# a =4
+# b=2
+# c=1
+# avg =(a+b+c)/3.0
+
+# print(avg)
+
+def average(a,b,c):
+    d = (a+b+c)/3.0
+    # print(d)
+    return d
+
+v1 = average(3,5,1)    
+v2 = average(4,2,1)
+
+print(v1)
+print(v2)
